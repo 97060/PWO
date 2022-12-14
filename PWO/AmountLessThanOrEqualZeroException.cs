f272100e7bf61982b7a3a264de4b6c85239d722e
v-ashka <mwijaszka99@gmail.com>
2022-12-14 19:24:41 +0100

    added Module3 and fixes

---
+++ PWO/AmountLessThanOrEqualZeroException.cs
@@ -0,0 +1,13 @@
+﻿namespace PWO
+{
+    /// <summary>
+    /// Klasa zawierająca wyjątek używany w module 3
+    /// </summary>
+    public class AmountLessThanOrEqualZeroException : Exception
+    {
+        /// <summary>
+        /// Wyjątek używany w module 3
+        /// </summary>
+        public AmountLessThanOrEqualZeroException() { }
+    }
+}
