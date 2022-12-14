3e38a79f081ff077c503122396009f314aae6aa5
grabol <s97060@pollub.edu.pl>
2022-12-14 18:42:54 +0100

    Moduł 1

---
+++ PWO/ExchangeRateClient.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+
+namespace PWO
+{
+    /// <summary>
+    /// Klasa obsługująca pobieranie i przetwarzanie danych do modułu 1
+    /// </summary>
+    public class ExchangeRateClient
+    {
+        private readonly NbpClient _client;
+
+        /// <summary>
+        /// Konstruktor klasy
+        /// </summary>
+        public ExchangeRateClient()
+        {
+            _client = new();
+        }
+
+        /// <summary>
+        /// Asynchroniczna funkcja pobierająca dane w postaci JSON i przetwarzająca je na tabice obiektów <see cref="Rate"/>Rate
+        /// </summary>
+        /// <returns>Zadanie reprezentujące asynchroniczne pobranie i przetwarzanie danych.
+        /// Resultat zawiera tablicę obiektów <see cref="Rate"/>Rate
+        /// </returns>
+        public async Task<Rate[]?> GetData()
+        {
+            var data = await _client.GetExchangeRateResponseAsync();
+            Rate[]? ratesArray = null;
+            var result = await JsonSerializer.DeserializeAsync<ExchangeRateData[]>(await data.Content.ReadAsStreamAsync());
+            if (result != null)
+                ratesArray = result[0].rates;
+            return ratesArray;
+        }
+    }
+}
---
+++ PWO/ExchangeRateData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PWO
+{
+    /// <summary>
+    /// Klasa reprezentująca format danych, do których serializowane są dane w postaci JSON
+    /// </summary>
+    public class ExchangeRateData
+    {
+        public Rate[] rates { get; set; }
+    }
+
+    /// <summary>
+    /// Klasa reprezentująca pojedyńczy obiekt w tablicy obiektów klasy <see cref="ExchangeRateData"/> ExchangeRateData
+    /// </summary>
+    public class Rate
+    {
+        public string currency { get; set; }
+        public string code { get; set; }
+        public float mid { get; set; }
+    }
+
+}
