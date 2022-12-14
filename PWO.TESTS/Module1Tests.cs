8e476f0b0086cc41f8c68d946680a0b197debadf
Sebastian Wiktor <103113980+93060@users.noreply.github.com>
2022-12-14 19:09:20 +0100

    Module 1

---
+++ PWO.TESTS/Module1Tests.cs
@@ -0,0 +1,33 @@
+﻿namespace PWO.TESTS
+{
+    public class Module1Tests
+    {
+
+        [Test] 
+        public async Task Response_StatusCode_Is_200()
+        {
+            NbpClient _nbpClient = new();
+            var actual = await _nbpClient.GetExchangeRateResponseAsync();
+            Assert.That(actual.IsSuccessStatusCode);
+        }
+
+        [Test]
+        public async Task Response_Is_JSON()
+        {
+            NbpClient _nbpClient = new();
+            var actual = await _nbpClient.GetExchangeRateResponseAsync();
+            Assert.That(actual.Content.Headers.ContentType, Is.Not.Null);
+            Assert.That(actual.Content.Headers.ContentType.MediaType, Is.EqualTo("application/json"));
+        }
+
+        [Test]
+        public async Task Response_Data_Not_Empty()
+        {
+            ExchangeRateClient _exchangeRate = new();
+            var actual = await _exchangeRate.GetData();
+            Assert.That(actual, Is.Not.Null);
+            Assert.That(actual.Count, Is.GreaterThan(0));
+        }
+
+    }
+}
--- PWO/Module1.Designer.cs
+++ PWO/Module1.Designer.cs
@@ -28,10 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(673, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(535, 492);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Module1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Module1";
+            this.Size = new System.Drawing.Size(1050, 498);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private Button button1;
+        private DataGridViewTextBoxColumn currencyDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn codeDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn midDataGridViewTextBoxColumn;
+        private DataGridView dataGridView1;
     }
 }
