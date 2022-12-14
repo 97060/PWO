1647c4a3afdd3eae8ac20ab3a3c728186b51ba09
grabol <s97060@pollub.edu.pl>
2022-12-14 18:40:59 +0100

    Add project files.

---
+++ PWO/Module1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PWO
+{
+    public partial class Module1 : UserControl
+    {
+        public Module1()
+        {
+            InitializeComponent();
+        }
+    }
+}
