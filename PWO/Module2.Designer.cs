387b9ac86d1e40b1ed8976a134ab2781098f86fd
97060 <103126350+97060@users.noreply.github.com>
2022-12-14 19:13:06 +0100

    Moduł 2

--- PWO/Module2.Designer.cs
+++ PWO/Module2.Designer.cs
@@ -28,10 +28,246 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.components = new System.ComponentModel.Container();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.typDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.opisDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.kwotaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.rejestrDataBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.kwotaTextBox = new System.Windows.Forms.TextBox();
+            this.opisTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.typOperacjiLabel = new System.Windows.Forms.Label();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.editModeCheckBox = new System.Windows.Forms.CheckBox();
+            this.saldoTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rejestrDataBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(372, 44);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 0;
+            this.saveButton.Text = "Zapisz";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.dataDataGridViewTextBoxColumn,
+            this.typDataGridViewTextBoxColumn,
+            this.opisDataGridViewTextBoxColumn,
+            this.kwotaDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.rejestrDataBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(606, 263);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.RowStateChanged += new System.Windows.Forms.DataGridViewRowStateChangedEventHandler(this.dataGridView1_RowStateChanged);
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            this.idDataGridViewTextBoxColumn.Visible = false;
+            this.idDataGridViewTextBoxColumn.Width = 42;
+            // 
+            // dataDataGridViewTextBoxColumn
+            // 
+            this.dataDataGridViewTextBoxColumn.DataPropertyName = "Data";
+            this.dataDataGridViewTextBoxColumn.HeaderText = "Data";
+            this.dataDataGridViewTextBoxColumn.Name = "dataDataGridViewTextBoxColumn";
+            this.dataDataGridViewTextBoxColumn.ReadOnly = true;
+            this.dataDataGridViewTextBoxColumn.Width = 56;
+            // 
+            // typDataGridViewTextBoxColumn
+            // 
+            this.typDataGridViewTextBoxColumn.DataPropertyName = "Typ";
+            this.typDataGridViewTextBoxColumn.HeaderText = "Typ";
+            this.typDataGridViewTextBoxColumn.Name = "typDataGridViewTextBoxColumn";
+            this.typDataGridViewTextBoxColumn.ReadOnly = true;
+            this.typDataGridViewTextBoxColumn.Width = 50;
+            // 
+            // opisDataGridViewTextBoxColumn
+            // 
+            this.opisDataGridViewTextBoxColumn.DataPropertyName = "Opis";
+            this.opisDataGridViewTextBoxColumn.HeaderText = "Opis";
+            this.opisDataGridViewTextBoxColumn.Name = "opisDataGridViewTextBoxColumn";
+            this.opisDataGridViewTextBoxColumn.ReadOnly = true;
+            this.opisDataGridViewTextBoxColumn.Width = 56;
+            // 
+            // kwotaDataGridViewTextBoxColumn
+            // 
+            this.kwotaDataGridViewTextBoxColumn.DataPropertyName = "Kwota";
+            this.kwotaDataGridViewTextBoxColumn.HeaderText = "Kwota (PLN)";
+            this.kwotaDataGridViewTextBoxColumn.Name = "kwotaDataGridViewTextBoxColumn";
+            this.kwotaDataGridViewTextBoxColumn.ReadOnly = true;
+            this.kwotaDataGridViewTextBoxColumn.Width = 98;
+            // 
+            // rejestrDataBindingSource
+            // 
+            this.rejestrDataBindingSource.DataSource = typeof(PWO.RejestrData);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "",
+            "WPŁYW",
+            "WYDATEK"});
+            this.comboBox1.Location = new System.Drawing.Point(6, 45);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // kwotaTextBox
+            // 
+            this.kwotaTextBox.Location = new System.Drawing.Point(239, 45);
+            this.kwotaTextBox.Name = "kwotaTextBox";
+            this.kwotaTextBox.PlaceholderText = "Kwota";
+            this.kwotaTextBox.Size = new System.Drawing.Size(100, 23);
+            this.kwotaTextBox.TabIndex = 4;
+            this.kwotaTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.kwotaTextBox_KeyPress);
+            this.kwotaTextBox.Leave += new System.EventHandler(this.kwotaTextBox_Leave);
+            // 
+            // opisTextBox
+            // 
+            this.opisTextBox.Location = new System.Drawing.Point(133, 45);
+            this.opisTextBox.Name = "opisTextBox";
+            this.opisTextBox.PlaceholderText = "Opis";
+            this.opisTextBox.Size = new System.Drawing.Size(100, 23);
+            this.opisTextBox.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.typOperacjiLabel);
+            this.groupBox1.Controls.Add(this.saveButton);
+            this.groupBox1.Controls.Add(this.kwotaTextBox);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.opisTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(3, 344);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(465, 74);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dodawanie";
+            // 
+            // typOperacjiLabel
+            // 
+            this.typOperacjiLabel.AutoSize = true;
+            this.typOperacjiLabel.ForeColor = System.Drawing.Color.Black;
+            this.typOperacjiLabel.Location = new System.Drawing.Point(6, 27);
+            this.typOperacjiLabel.Name = "typOperacjiLabel";
+            this.typOperacjiLabel.Size = new System.Drawing.Size(70, 15);
+            this.typOperacjiLabel.TabIndex = 8;
+            this.typOperacjiLabel.Text = "Typ operacji";
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(615, 243);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(75, 23);
+            this.DeleteButton.TabIndex = 6;
+            this.DeleteButton.Text = "Usuń";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // editModeCheckBox
+            // 
+            this.editModeCheckBox.AutoSize = true;
+            this.editModeCheckBox.Location = new System.Drawing.Point(615, 218);
+            this.editModeCheckBox.Name = "editModeCheckBox";
+            this.editModeCheckBox.Size = new System.Drawing.Size(82, 19);
+            this.editModeCheckBox.TabIndex = 7;
+            this.editModeCheckBox.Text = "Tryb edycji";
+            this.editModeCheckBox.UseVisualStyleBackColor = true;
+            this.editModeCheckBox.CheckedChanged += new System.EventHandler(this.editModeCheckBox_CheckedChanged);
+            // 
+            // saldoTextBox
+            // 
+            this.saldoTextBox.Location = new System.Drawing.Point(60, 272);
+            this.saldoTextBox.Name = "saldoTextBox";
+            this.saldoTextBox.Size = new System.Drawing.Size(100, 23);
+            this.saldoTextBox.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 275);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Saldo:";
+            // 
+            // Module2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.saldoTextBox);
+            this.Controls.Add(this.editModeCheckBox);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Module2";
+            this.Size = new System.Drawing.Size(866, 421);
+            this.Load += new System.EventHandler(this.Module2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rejestrDataBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button saveButton;
+        private DataGridView dataGridView1;
+        private BindingSource bindingSource1;
+        private BindingSource rejestrDataBindingSource;
+        private ComboBox comboBox1;
+        private TextBox kwotaTextBox;
+        private TextBox opisTextBox;
+        private GroupBox groupBox1;
+        private Button DeleteButton;
+        private CheckBox editModeCheckBox;
+        private Label typOperacjiLabel;
+        private DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn dataDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn typDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn opisDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn kwotaDataGridViewTextBoxColumn;
+        private TextBox saldoTextBox;
+        private Label label2;
     }
 }
--- PWO/Module2.cs
+++ PWO/Module2.cs
@@ -12,9 +12,148 @@ namespace PWO
 {
     public partial class Module2 : UserControl
     {
-        public Module2()
+        private readonly Db client = new();
+       public Module2()
         {
             InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (!Validate())
+                return;
+            if (editModeCheckBox.Checked)
+            {
+                await client.EditDataAsync((int)dataGridView1.SelectedRows[0].Cells[0].Value, comboBox1.Text, opisTextBox.Text, Convert.ToDecimal(kwotaTextBox.Text));
+            }
+            else
+            {
+                await client.AddDataAsync(comboBox1.Text, opisTextBox.Text, Convert.ToDecimal(kwotaTextBox.Text));
+            }
+            await RefreshAsync();
+        }
+
+        private void Module2_Load(object sender, EventArgs e)
+        {
+            RefreshAsync();
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            await client.DeleteDataAsync((int)dataGridView1.SelectedRows[0].Cells[0].Value);
+            await RefreshAsync();
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (!editModeCheckBox.Checked)
+                return;
+            SetControlsData();
+        }
+
+        private void editModeCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (editModeCheckBox.Checked)
+            {
+                groupBox1.Text = "Edytuj";
+                if (dataGridView1.SelectedRows.Count > 0)
+                    SetControlsData();
+            }
+            else
+            {
+                groupBox1.Text = "Dodaj";
+                ClearControls();
+            }
+        }
+
+        private void dataGridView1_RowStateChanged(object sender, DataGridViewRowStateChangedEventArgs e)
+        {
+            if (e.StateChanged != DataGridViewElementStates.Selected || !editModeCheckBox.Checked || dataGridView1.SelectedRows.Count == 0)
+                return;
+            SetControlsData();
+        }
+
+        private void kwotaTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string text = (sender as TextBox).Text;
+            e.Handled = e.KeyChar != (char)8 && !char.IsDigit(e.KeyChar) && e.KeyChar != '.';
+
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+
+            if (!char.IsControl(e.KeyChar) && text.Contains('.') && text[text.IndexOf('.')..].Length > 2)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void kwotaTextBox_Leave(object sender, EventArgs e)
+        {
+            string text = (sender as TextBox).Text;
+            if (text.EndsWith('.'))
+                kwotaTextBox.Text = text.Remove(text.IndexOf('.'));
+        }
+
+        private async Task RefreshAsync()
+        {
+            rejestrDataBindingSource.DataSource = await client.GetDataAsync();
+            DeleteButton.Enabled = false;
+            editModeCheckBox.Enabled = false;
+            saldoTextBox.Text = String.Empty;
+            if (dataGridView1.RowCount > 0)
+            {
+                DeleteButton.Enabled = true;
+                editModeCheckBox.Enabled = true;
+                dataGridView1.FirstDisplayedScrollingRowIndex = dataGridView1.RowCount - 1;
+                saldoTextBox.Text = dataGridView1.Rows.Cast<DataGridViewRow>()
+                    .Sum(x => (decimal)x.Cells[4].Value).ToString() + " PLN";
+            }
+            ClearControls();
+        }
+
+        private bool Validate()
+        {
+            string message = "";
+            if (String.IsNullOrEmpty(comboBox1.Text))
+            {
+                message += "Należy wybrać typ operacji.\n";
+            }
+            if (String.IsNullOrEmpty(opisTextBox.Text))
+            {
+                message += "Należy uzupełnić opis.\n";
+            }
+            if (String.IsNullOrEmpty(kwotaTextBox.Text))
+            {
+                message += "Kwota nie może być pusta.\n";
+            }
+
+            if (!String.IsNullOrEmpty(message))
+            {
+                MessageBox.Show(message, "Walidacja danych");
+            }
+            return String.IsNullOrEmpty(message);
+        }
+
+        private void SetControlsData()
+        {
+            var selectedRowCells = dataGridView1.SelectedRows[0].Cells;
+            comboBox1.SelectedIndex = comboBox1.FindStringExact(selectedRowCells[2].Value.ToString());
+            opisTextBox.Text = selectedRowCells[3].Value.ToString();
+            kwotaTextBox.Text = selectedRowCells[4].Value.ToString();
+        }
+        private void ClearControls()
+        {
+            comboBox1.SelectedIndex = 0;
+            opisTextBox.Text = String.Empty;
+            kwotaTextBox.Text = String.Empty;
         }
     }
 }
